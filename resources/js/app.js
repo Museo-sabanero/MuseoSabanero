a75af799dd2c3e29f0f3f95e678bd68687ef9419
MayderC <maydercortes1@gmail.com>
2023-11-22 03:05:42 -0600

    fix appjs, resources vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,10 +3,6 @@ require('./bootstrap')
 import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
-<<<<<<< HEAD
 import { store } from './store/index.js'
-=======
-import store from './store/index'
->>>>>>> development
 
 createApp(App).use(router).use(store).mount('#app')
